8e2e38fdc335bc7929a854d15d86c0afde2d9f57
john.xiong <xiongnanbin@126.com>
2018-07-23 16:04:10 +0800

    member ship 用户管理

---
+++ eMusicStore.Web/Controllers/StoreController.cs
@@ -0,0 +1,33 @@
+﻿using eMusicStore.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eMusicStore.Web.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MusicStoreEntities _storeDB = new MusicStoreEntities();
+        // GET: Store
+        public ActionResult Index()
+        {
+            var genres = _storeDB.Genres.ToList();
+            return View(genres);
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            ViewBag.Genre = genre;
+            var albums = _storeDB.Albums.Where(a => a.Genre.Name == genre).ToList();
+            return View(albums);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var album = _storeDB.Albums.Find(id);
+            return View(album);
+        }
+    }
+}
---
+++ eMusicStore.Web/Models/Cart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace eMusicStore.Web.Models
+{
+    /// <summary>
+    /// 购物车
+    /// </summary>
+    public class Cart
+    {
+        [Key]
+        public int RecordId { get; set; }
+        public string CartId { get; set; }
+        public int AlbumId { get; set; }
+        public int Count { get; set; }
+        public DateTime DateCreated { get; set; }
+        public virtual Album Album { get; set; }
+    }
+}
